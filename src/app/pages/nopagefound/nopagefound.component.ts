47adac464940ac3d0607b1166f4adad3f9e9fb3c
Diego <dcastroaurora@gmail.com>
2021-01-24 18:30:51 -0500

    Diseño listo

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.scss'],
+})
+export class NopagefoundComponent {
+  year = new Date().getFullYear();
+}
